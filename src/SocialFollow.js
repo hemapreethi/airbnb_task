691d21dde6271687c174c86ec9df72ba61faf2b7
hemapreethi <42565862+hemapreethi@users.noreply.github.com>
2020-07-30 16:08:23 +0530

    Update SocialFollow.js

--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -4,43 +4,33 @@ import {
   faFacebook,
   faTwitter,
   faInstagram,
-faSearch
 } from "@fortawesome/free-brands-svg-icons";
-class Search extends React.Component{
-render(){
-  return (
-<div>
-<a href="https://www.facebook.com/learnbuildteach/"
-  className="facebook social">
-  <FontAwesomeIcon icon={faFacebook} size="1x" />
-</a>
-</div>
-);
-}
-}
+import globe2 from  './Images/globe.png';
+import'./index.css';
+import './index.scss';
 class Fb extends React.Component{
 render(){
   return (
-<ul >
-<li>
+
+<ul style={{listStyle:'none',display:'inline',paddingLeft:'10px'}}>
+<li  style={{paddingLeft:'10px'}}><a href="#" style={{fontSize:'16px',color:'black'}} ><img src={globe2} style={{width:'20px',height:'20px'}}/> ENGLISH(IN)</a></li>
+<li style={{paddingLeft:'10px'}}><a href="#" style={{fontSize:'16px',color:'black'}}>&#8377; </a> INR</li>
+<li style={{paddingLeft:'10px',paddingRight:'10px'}}>
 <a href="https://www.facebook.com/learnbuildteach/"
-  className="facebook social">
+  className="facebook social" style={{fontSize:'16px',color:'black'}}>
   <FontAwesomeIcon icon={faFacebook} size="1x" />
 </a></li>
-<li>
-<a href="https://www.twitter.com/jamesqquick" className="twitter social">
+<li style={{paddingLeft:'10px',paddingRight:'10px'}}>
+<a href="https://www.twitter.com/jamesqquick" className="twitter social" style={{fontSize:'16px',color:'black'}}>
   <FontAwesomeIcon icon={faTwitter} size="1x" />
 </a></li>
-<li>
+<li style={{paddingLeft:'10px',paddingRight:'10px'}}>
 <a href="https://www.instagram.com/learnbuildteach"
-  className="instagram social">
+  className="instagram social" style={{fontSize:'16px',color:'black'}}>
   <FontAwesomeIcon icon={faInstagram} size="1x" />
 </a>
 </li>
 </ul>
-
   );
 }
 }export default Fb;
-
-ReactDOM.render(<Search />, document.getElementById('search'));
