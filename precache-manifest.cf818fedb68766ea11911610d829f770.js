e86bc97e319f2d31d3b648de3b128ed859bb6774
hema preethi <hemapreethidevaraj@gmail.com>
2020-07-30 09:13:43 +0530

    Updates

---
+++ precache-manifest.cf818fedb68766ea11911610d829f770.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b385404f92525d2c51e34ba005fb765a",
+    "url": "/airbnb_task/index.html"
+  },
+  {
+    "revision": "93088ca06ae1d1957bd5",
+    "url": "/airbnb_task/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "40b1e4f8c5eaec36f99c",
+    "url": "/airbnb_task/static/css/main.799b6f61.chunk.css"
+  },
+  {
+    "revision": "93088ca06ae1d1957bd5",
+    "url": "/airbnb_task/static/js/2.f4b1d0e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/airbnb_task/static/js/2.f4b1d0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b1e4f8c5eaec36f99c",
+    "url": "/airbnb_task/static/js/main.8c0edad2.chunk.js"
+  },
+  {
+    "revision": "862b4ad23409afc5d6ad",
+    "url": "/airbnb_task/static/js/runtime-main.3522cc8a.js"
+  },
+  {
+    "revision": "106c1201de17fbd75efa71181560ef09",
+    "url": "/airbnb_task/static/media/airbnb.106c1201.png"
+  },
+  {
+    "revision": "6a1d6bbd0994e95c2a383cb699c144c1",
+    "url": "/airbnb_task/static/media/art of reinvention with simidele adeagbo.6a1d6bbd.jpg"
+  },
+  {
+    "revision": "36998cbc46adb52404593f5ea307e101",
+    "url": "/airbnb_task/static/media/entire homes.36998cbc.jpg"
+  },
+  {
+    "revision": "9cc60e4938e57a67b73fca2c0fceed8e",
+    "url": "/airbnb_task/static/media/explore surf life with jordy smith.9cc60e49.webp"
+  },
+  {
+    "revision": "88b38ff9ee26b82ce32096a7a0a66d4b",
+    "url": "/airbnb_task/static/media/globe.88b38ff9.png"
+  },
+  {
+    "revision": "43d973ac334ab2d7976e14c11c796d27",
+    "url": "/airbnb_task/static/media/globe1.43d973ac.jpg"
+  },
+  {
+    "revision": "d5a6301a97244f46985dc4e9566542a4",
+    "url": "/airbnb_task/static/media/mindful movement with adam kellerman.d5a6301a.jfif"
+  },
+  {
+    "revision": "1a82598ada4929b7e12c19062884b6fd",
+    "url": "/airbnb_task/static/media/olympian and paralympian online experiences.1a82598a.jpg"
+  },
+  {
+    "revision": "3e4d70881776487b713b7c2a334acf94",
+    "url": "/airbnb_task/static/media/olympian and paralympian online experiences.3e4d7088.webp"
+  },
+  {
+    "revision": "c6ecd12cc2020c2fec6b336537a43ea2",
+    "url": "/airbnb_task/static/media/olympic.c6ecd12c.jpg"
+  },
+  {
+    "revision": "cc35426cd4b7c51808184c6023eaeeb0",
+    "url": "/airbnb_task/static/media/online experiences.cc35426c.webp"
+  },
+  {
+    "revision": "26ba7880ddd9152820c1f2d67cf7c7e3",
+    "url": "/airbnb_task/static/media/search.26ba7880.svg"
+  },
+  {
+    "revision": "7c087a07bd549f4b110b557a4ab54766",
+    "url": "/airbnb_task/static/media/user.7c087a07.svg"
+  },
+  {
+    "revision": "86bbcf391135858a790508f18f3127f9",
+    "url": "/airbnb_task/static/media/your next getaway.86bbcf39.webp"
+  }
+]);
